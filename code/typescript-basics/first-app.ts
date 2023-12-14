bb193541ccd727319c73faeef34cdd3eb1349357
inodw1 <inodwagachchi@gmail.com>
2023-12-14 15:27:34 +0530

    Basic primitive types

--- code/typescript-basics/first-app.ts
+++ code/typescript-basics/first-app.ts
@@ -2,4 +2,16 @@ let userName0: string;
 userName0 = "Inod";
 
 // let userName0 = 23;
+// let userName = "Inod";
+
+/**
+ * Primitive types
+ * we can follow below both patterns
+ */
+
+// let userName: string = "Inod";
 let userName = "Inod";
+// let userAge: number = 31;
+let userAge = 31;
+// let isValid: boolean = true;
+let isValid = true;
