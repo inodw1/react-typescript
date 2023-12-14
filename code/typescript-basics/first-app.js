cf42d44c8fa5a77bd4457b6bfe27f050173ad1fd
inodw1 <inodwagachchi@gmail.com>
2023-12-14 15:43:28 +0530

    Invoke the typescript compiler

---
+++ code/typescript-basics/first-app.js
@@ -0,0 +1,14 @@
+var userName0;
+userName0 = "Inod";
+// let userName0 = 23;
+// let userName = "Inod";
+/**
+ * Primitive types
+ * we can follow below both patterns
+ */
+// let userName: string = "Inod";
+var userName = "Inod";
+// let userAge: number = 31;
+var userAge = 31;
+// let isValid: boolean = true;
+var isValid = true;
--- code/typescript-basics/first-app.ts
+++ code/typescript-basics/first-app.ts
@@ -6,7 +6,7 @@ userName0 = "Inod";
 
 /**
  * Primitive types
- * we can follow below both patterns
+ * we can follow below both
  */
 
 // let userName: string = "Inod";
@@ -15,3 +15,12 @@ let userName = "Inod";
 let userAge = 31;
 // let isValid: boolean = true;
 let isValid = true;
+
+/**
+ * Browser only knows about js, then we need to compile this ts code to js
+ * we can use npx tsc FILENAME.ts (if already have install tsc in globally we can use tsc FILENAME.ts)
+ * Then it is create same code with javascript
+ * we can see, it has remove type annotations and converted in to a bascic js code
+ * But we are not going to compile every time
+ * instead of, we can identify erros with support of the IDE
+ */
